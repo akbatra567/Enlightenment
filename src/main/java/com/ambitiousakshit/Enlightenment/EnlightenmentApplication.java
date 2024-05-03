9ac0bc821a13bd35fe141f92b0b7a404fa2910ab
Akshit Batra <akbatra567@gmail.com>
2024-05-04 04:21:12 +0530

    initial commit

---
+++ src/main/java/com/ambitiousakshit/Enlightenment/EnlightenmentApplication.java
@@ -0,0 +1,13 @@
+package com.ambitiousakshit.Enlightenment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnlightenmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnlightenmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ambitiousakshit/Enlightenment/EnlightenmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ambitiousakshit.Enlightenment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnlightenmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
